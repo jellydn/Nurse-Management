9364171518cbe9dc1a32800e857b45ec59295940
TienLP <tienlp@Les-Mac-mini.local>
2014-02-13 17:18:54 +0700

    [Duc.vov]List shift

---
+++ Nurse Management/Classess/Controllers/iPhone/ShiftCell.m
@@ -0,0 +1,17 @@
+//
+//  ShiftCell.m
+//  Nurse Management
+//
+//  Created by Le Phuong Tien on 2/13/14.
+//  Copyright (c) 2014 Le Phuong Tien. All rights reserved.
+//
+
+#import "ShiftCell.h"
+
+@interface ShiftCell ()
+
+@end
+
+@implementation ShiftCell
+
+@end
