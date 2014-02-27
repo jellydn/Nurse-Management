1fe6a1f8af50b8b3b98efcc396838e3298a8ad1a
tienlp <tienlp@webdev>
2014-02-27 15:29:48 +0700

    [TienLP] MemberListVC --> fix desgin

---
+++ Nurse Management/Classess/Controllers/iPhone/MemberCell.h
@@ -0,0 +1,16 @@
+//
+//  MemberCell.h
+//  Nurse Management
+//
+//  Created by Le Phuong Tien on 2/27/14.
+//  Copyright (c) 2014 Le Phuong Tien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MemberCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UILabel    *lbTitle;
+@property (nonatomic, weak) IBOutlet UISwitch   *switchView;
+
+@end
---
+++ Nurse Management/Classess/Controllers/iPhone/MemberCell.m
@@ -0,0 +1,18 @@
+//
+//  MemberCell.m
+//  Nurse Management
+//
+//  Created by Le Phuong Tien on 2/27/14.
+//  Copyright (c) 2014 Le Phuong Tien. All rights reserved.
+//
+
+#import "MemberCell.h"
+
+@interface MemberCell ()
+
+@end
+
+@implementation MemberCell
+
+
+@end
